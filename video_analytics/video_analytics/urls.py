8239f30f17487b67250dd7881c30d216e3cf63b9
Александр Жданов <canshok89@gmail.com>
2023-11-30 23:00:50 +0500

    Added urls

--- video_analytics/video_analytics/urls.py
+++ video_analytics/video_analytics/urls.py
@@ -14,16 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('about/', include('about.urls', namespace='about')),
     path('', include('video.urls', namespace='videos')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
+handler403 = 'core.views.permission_denied'
+handler403csrf = 'core.views.csrf_failure'
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
 
 # Эти строки — в самый конец файла:
 if settings.DEBUG:
