d0e0deda6dc6977ca39843441f15d4580f497ace
Александр Жданов <canshok89@gmail.com>
2024-02-04 18:07:44 +0500

    Commented video forms

--- video_analytics/video/forms.py
+++ video_analytics/video/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from video.models import Video
 
 
-class VideoForm(forms.ModelForm):
-    class Meta:
-        model = Video
-        fields = ('__all__',)
+# class VideoForm(forms.ModelForm):
+#     class Meta:
+#         model = Video
+#         fields = ('__all__',)
