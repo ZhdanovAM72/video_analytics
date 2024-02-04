5590f3e7b2ae8b0f4a01b43eb16703c3ac098435
Александр Жданов <canshok89@gmail.com>
2024-02-04 19:13:54 +0500

    Added new url

--- video_analytics/video/urls.py
+++ video_analytics/video/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # Для открытия видео отдельно
     path('stream/<int:pk>/', views.get_streaming_video, name='stream'),
     path('video/<int:pk>/', views.get_video, name='video'),
+    path('video/create/', views.create_video, name='create_video'),
     # path('<int:pk>/', views.VideoDetailView, name='video'),
     # path('', views.get_list_video, name='index'),
     path('', views.index, name='index'),
