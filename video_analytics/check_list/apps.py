97d32fe9b8268bad0df39afa901fa815d95aa9c3
Александр Жданов <canshok89@gmail.com>
2025-01-15 15:31:35 +0500

    Added max-line-length

--- video_analytics/check_list/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CheckListConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'check_list'
--- video_analytics/core/context_processors/year.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-
-
-def year(request):
-    """Добавляет переменную с текущим годом."""
-    today = dt.date.today()
-    return {
-        'year': int(today.strftime('%Y'))
-    }
